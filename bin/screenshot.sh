a6d5d21d333a292f72ac4bc8d35ee519ca7686e4
Henrik Holst <hholst80@gmail.com>
2017-09-27 11:49:59 +0200

    update screenshot.sh

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# exec >& $0.log
+
+FILENAME=`mktemp -p /tmp tmp.XXXXXX.png`
+function finish {
+  rm -rf $FILENAME
+}
+trap finish EXIT
+
 case $1 in
+	all)
+		scrot -u $FILENAME
+		xclip -selection clipboard -t image/png < $FILENAME
+		;;
 	win|window)
-		maim -u -i $(xdotool selectwindow) | xclip -selection clipboard -t image/png
+		# WORKAROUND:
+		# https://stackoverflow.com/questions/35500163/bash-script-with-scrot-area-not-working
+		sleep 0.2
+		scrot -s $FILENAME
+		xclip -selection clipboard -t image/png < $FILENAME
 		;;
 	sel|select|selection)
-		maim -u -s | xclip -selection clipboard -t image/png
+		scrot -s $FILENAME
+		xclip -selection clipboard -t image/png < $FILENAME
 		;;
 	*)
 		;;
