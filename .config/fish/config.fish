115ef2c4b95117bfaa408fb516a05efff1b0ecb2
Henrik Holst <hholst80@users.noreply.github.com>
2020-03-18 23:46:49 +0100

    Update fish config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,4 @@ function _docker_ps
 	docker ps --format "{{.Image|printf \"%-35v\"}}{{.Status|printf \"%-15v\"}}{{.Names|printf \"%25v\"}}"
 end
 bind \cp "watch -x fish -c _docker_ps; commandline -f repaint"
-git config --list | sed -n 's/^alias\.\(.*\)=\(.*\)$/abbr -ag \1 "git \2"/p' | sed -n '/!/!p' | source
+git config --list | sed -n 's/^alias\.\(.*\)=\(.*\)$/abbr -ag g\1 "git \2"/p' | sed -n '/!/!p' | source
